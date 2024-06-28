f9daa85b259bd2c3d38da5140f2320fcdb3fea45
lyeska6 <liz-on-chik@mail.ru>
2024-06-28 07:35:56 +0300

    1

---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,20 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity:  AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+
+        val arrowBackButton: ImageView = findViewById<ImageView>(R.id.arrowBack)
+        arrowBackButton.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
