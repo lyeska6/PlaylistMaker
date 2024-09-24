83a731b2b6b77ad59db1f37e0e27506fc8616350
lyeska6 <65342123+lyeska6@users.noreply.github.com>
2024-09-24 22:40:44 +0300

    Revert "1"

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,34 +1,20 @@
 package com.example.playlistmaker
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        window.statusBarColor = ContextCompat.getColor(this, R.color.main_screen_color)
-
-        val butSearch = findViewById<Button>(R.id.butSearch)
-        butSearch.setOnClickListener{
-            startActivity(Intent(this, SearchActivity::class.java))
-        }
-
-        val butMedia = findViewById<Button>(R.id.butMedia)
-        butMedia.setOnClickListener{
-            val mediaIntent: Intent = Intent(this, MediaActivity::class.java)
-            startActivity(mediaIntent)
-        }
-
-        val butSettings = findViewById<Button>(R.id.butSettings)
-        butSettings.setOnClickListener{
-            startActivity(Intent(this, SettingsActivity::class.java))
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
