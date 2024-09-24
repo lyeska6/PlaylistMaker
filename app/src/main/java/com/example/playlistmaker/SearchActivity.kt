7e9cd180f4639c2994df36ee7daaf9b2e2031d28
lyeska <liz-on-chik@mail.ru>
2024-09-24 16:57:52 +0300

    экран поиска и его работа

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,15 +1,69 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_search)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.screen_color)
+
+        if (savedInstanceState != null) {
+            textSearch = savedInstanceState.getString(INPUT_SEARCH, TEXT_SEARCH)
+        }
+
+        val inputSearch = findViewById<EditText>(R.id.searchInput)
+        val emptySearch = findViewById<ImageView>(R.id.emptySearch)
+
+        inputSearch.setText(textSearch)
+
+        emptySearch.setOnClickListener{
+            inputSearch.setText("")
+        }
+
+        val textListener = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //none
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                emptySearch.visibility = emptyButVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                textSearch = s.toString()
+            }
+        }
+        inputSearch.addTextChangedListener(textListener)
+    }
+
+    private fun emptyButVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    private var textSearch : String = TEXT_SEARCH
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(INPUT_SEARCH, textSearch)
+    }
+
+    companion object {
+        const val INPUT_SEARCH = "INPUT_SEARCH"
+        const val TEXT_SEARCH = ""
     }
 }
